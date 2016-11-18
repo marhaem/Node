ddd874e9041047fa6fa805f4be98b47a5dfc0eb4
marhaem <marhaem@emm.mobi>
2016-11-18 16:08:55 +0100

    modularized some code

--- projekt/webserver/src/lib/WebServer/Global.js
+++
@@ -1,6 +0,0 @@
-/**
- * Here some objects are stored which need to be accesible to the WebServer globally
- */
-
-export default {
-};
--- projekt/webserver/src/lib/WebServer/Sequelize/import.js
+++
@@ -1,5 +0,0 @@
-
-/*global System*/
-export default function _import() {
-  return System.import('sequelize');
-}
